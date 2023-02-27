72456de1b84cabd8e44cbed3f7eb2ccbaafb3766
Corey Zhou <95654612+co-zhou@users.noreply.github.com>
2023-02-27 15:49:02 -0800

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/co-zhou/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('coreyzh@gmail.com')
         }
